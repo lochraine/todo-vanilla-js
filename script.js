23b4c6c2996e9593aa9efd0451b2b51c2a24bad3
lochraine <lochraine@hotmail.co.uk>
2019-01-17 20:10:40 +0000

    Update script.js

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const uncheckedCountSpan = document.getElementById('unchecked-count')
 function createListElement() {
 	li = document.createElement("li");
 	li.setAttribute("class", classNames.TODO_ITEM)
-const checker = document.createElement("input")
-checker.setAttribute("type", "checkbox")
-checker.setAttribute("class", classNames.TODO_CHECKBOX)
+	const checker = document.createElement("input")
+	checker.setAttribute("type", "checkbox")
+	checker.setAttribute("class", classNames.TODO_CHECKBOX)
 	console.log(document.getElementById('input field').value)
 	li.appendChild(document.createTextNode(document.getElementById('input field').value));
 	li.appendChild(checker)
